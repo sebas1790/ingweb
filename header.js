67abbe37361f8cbf1514c54ea09af36577e9a028
Sala_14 <Sala_14@csi.ucentral.edu.co>
2019-04-27 13:04:00 -0500

    Commit inicial

---
+++ header.js
@@ -0,0 +1,63 @@
+document.write('
+	<div id="myTopnav" class="row header topnav">
+			<div class="topnavMobile row">
+	    		<a class="nav-link dropdown-toggle col" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
+	          		Men&uacute;
+	        	</a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="#">Consultar Usuarios</a>
+		          <a class="dropdown-item" href="#">Crear usuario</a>
+		          <a class="dropdown-item" href="#">Eliminar usuario</a>
+		          <a class="dropdown-item" href="#">Cerrar sesi&oacute;n</a>
+		        </div>
+		        <div class="topnavMobile col">
+		        	<a href="#">
+						<figure>
+							<img src="images/icons/svgs/solid/home.svg" alt="home" style="width: 100%; margin-left: 100%;">
+						</figure>
+					</a>
+		        </div>
+			</div>
+			<div class="col desktop">
+				<a href="#">
+					<figure>
+						<img src="images/icons/svgs/solid/home.svg" alt="home">
+					</figure>
+					<p>Inicio</p>
+				</a>
+			</div>
+			<div class="col desktop">
+				<a href="usuarios.html">
+					<figure>
+						<img src="images/icons/svgs/solid/users.svg" alt="users">
+					</figure>
+					<p>Consultar usuarios</p>
+				</a>
+			</div>
+
+			<div class="col desktop">
+				<a href="#">
+					<figure>
+						<img src="images/icons/svgs/solid/user-plus.svg" alt="create user">
+					</figure>
+					<p>Crear usuario</p>
+				</a>
+			</div>
+			<div class="col desktop">
+				<a href="#">
+					<figure>
+						<img src="images/icons/svgs/solid/user-times.svg" alt="delete user">
+					</figure>
+					<p>Eliminar Usuario</p>
+				</a>
+			</div>
+			<div class="col desktop">
+				<a href="#">
+					<figure>
+						<img src="images/icons/svgs/solid/sign-out-alt.svg" alt="logout">
+					</figure>
+					<p>Cerrar sesi&oacute;n</p>
+				</a>
+			</div>
+		</div>	
+/);
